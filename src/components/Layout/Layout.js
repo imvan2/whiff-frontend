458d16ad99bc1a7ade6bcbc507ed0d71499d05fd
Van Tu <vtu7410@gmail.com>
2024-05-22 15:51:50 -0700

    did some things for the search page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,74 @@
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
 import SearchBar from "./SearchBar/SearchBar";
+import { useState, useEffect } from "react";
 
 function Layout() {
+  // State to hold perfumes and designers
+  const [perfumes, setPerfumes] = useState([]);
+  const [designers, setDesigner] = useState([]);
+
+  /* savingDesignerName is an arrow function that loop over the perfumes and designers
+   * objects to match designers with the perfumes.
+   * Sets the changed perfumes object as state
+   *
+   * Data fetched from backend has perfumes[0].designer as an ID that references designers object ID*/
+  const savingDesignerName = (designers, perfumes) => {
+    for (let i = 0; i < perfumes.length; i++) {
+      for (let j = 0; j < designers.length; j++) {
+        if (perfumes[i]["designer"] === designers[j]["id"]) {
+          perfumes[i]["designer"] = designers[j]["name"];
+        }
+      }
+    }
+    setPerfumes(perfumes);
+  };
+
+  /* fetchingData is an async arrow function that gets data from the backend
+   * Converts the response to JSON
+   * Sets the fetched data as state
+   */
+  const fetchingData = async () => {
+    const perfumeResponse = await fetch(
+      `https://whiff-backend-5f278bf19e19.herokuapp.com/api/perfume`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const perfumeData = await perfumeResponse.json();
+
+    const designersResponse = await fetch(
+      `https://whiff-backend-5f278bf19e19.herokuapp.com/api/designers`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const designerData = await designersResponse.json();
+    setPerfumes(perfumeData);
+    setDesigner(designerData);
+  };
+
+  /* Effect hook to fetch data immediately upon loading the page */
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  useEffect(() => {
+    // Run savingDesignerName function when perfumes or designers change
+    savingDesignerName(designers, perfumes);
+  }, [designers]);
+
   return (
     <>
       <Header />
       <SearchBar />
-      <Outlet />
+      {perfumes && <Outlet context={[perfumes, designers]} />}
       {/* <Footer /> */}
     </>
   );
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -2,82 +2,100 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import PerfumeIcon from "../HomePage/components/PerfumeIcon";
+import { useOutletContext } from "react-router-dom";
 
+// TODO: search by full name ex "angel by mugler"
+// TODO: search by brand name ex "mugler"
+// TODO: change the search capitilization ex "angel" to "Angel"
+// TODO: change the caps on multiple words
+
+/** SearchPage is a react component that renders search results
+ * based on the search input from SearchBar.js
+ *
+ * No props
+ * Gets two states from parent component: search input and perfumes object
+ **/
 function SearchPage() {
+  // Gets the state using React location comes from SearchBar.js navigate
   let location = useLocation();
-  console.log("location:", location.state);
 
-  const [perfumes, setPerfumes] = useState([]);
-  const [designers, setDesigner] = useState([]);
-  const perfumeResults = [];
+  let searchName = "";
+  let searchBrand = "";
+  let searchArray = [];
+  let search = "";
 
-  // TODO: instead of looping, match the id of the designer
-  const savingDesignerName = (designers, perfumes) => {
-    for (let i = 0; i < perfumes.length; i++) {
-      for (let j = 0; j < designers.length; j++) {
-        if (perfumes[i]["designer"] === designers[j]["id"]) {
-          perfumes[i]["designer"] = designers[j]["name"];
-        }
+  // TODO: is there a way to refactor this so it's not so long?
+  // TODO: maybe keep the split as a list, then compare the search against that list
+  // Checking if the string contains "by"
+  // Break apart the string to search
+  if (location.state.includes(" by ")) {
+    searchName = location.state.split(" by ")[0];
+    searchBrand = location.state.split(" by ")[1];
+
+    // Capitalizing the first letter of the string
+    searchName = searchName.toLowerCase();
+    searchName = searchName.charAt(0).toUpperCase() + searchName.slice(1);
+
+    searchBrand = searchBrand.toLowerCase();
+    searchBrand = searchBrand.charAt(0).toUpperCase() + searchBrand.slice(1);
+  } else {
+    searchArray = location.state.split(" ");
+
+    if (searchArray.length > 0) {
+      for (let i = 0; i < searchArray.length; i++) {
+        searchArray[i] = searchArray[i].toLowerCase();
+        searchArray[i] =
+          searchArray[i].charAt(0).toUpperCase() + searchArray[i].slice(1);
       }
+      search = searchArray.join(" ");
+    } else {
+      search = location.state.toLowerCase();
+      search = search.charAt(0).toUpperCase() + search.slice(1);
     }
-    setPerfumes(perfumes);
-  };
+  }
 
-  const fetchingData = async () => {
-    const perfumeResponse = await fetch(
-      `https://whiff-backend-5f278bf19e19.herokuapp.com/api/perfume`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const perfumeData = await perfumeResponse.json();
+  // Gets the state from Outlet in Landing.js
+  const [perfumes, designers] = useOutletContext();
 
-    const designersResponse = await fetch(
-      `https://whiff-backend-5f278bf19e19.herokuapp.com/api/designers`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const designerData = await designersResponse.json();
-    setPerfumes(perfumeData);
-    setDesigner(designerData);
-  };
+  // Sets state of perfume results
+  const [perfumeResults, setPerfumeResults] = useState([]);
 
   const searchResults = () => {
+    let matches = [];
+
     for (let i = 0; i < perfumes.length; i++) {
-      console.log(perfumes[i]["name"].includes(location.state));
-      if (perfumes[i]["name"].includes(location.state)) {
-        perfumeResults.push(perfumes[i]);
+      // console.log(
+      //   "is this true or false",
+      //   perfumes[i]["name"].includes(location.state)
+      // );
+      if (
+        perfumes[i]["name"] === searchName &&
+        perfumes[i]["designer"] === searchBrand
+      ) {
+        matches.push(perfumes[i]);
+      } else if (perfumes[i]["name"].includes(search)) {
+        matches.push(perfumes[i]);
+      } else if (perfumes[i]["designer"].includes(search)) {
+        matches.push(perfumes[i]);
       }
     }
-    console.log(perfumeResults);
+    setPerfumeResults(matches);
   };
-
-  useEffect(() => {
-    fetchingData();
-  }, []);
 
   useEffect(() => {
     // Run savingDesignerName function when perfumes or designers change
-    savingDesignerName(designers, perfumes);
+    // savingDesignerName(designers, perfumes);
     searchResults();
-  }, [designers, perfumeResults, location.state]);
-
-  // TODO: iterate over perfumes and find if there's a match
+  }, [perfumes, location.state]);
 
   return (
     <>
       <div>
-        {perfumeResults.length > 0 ? (
+        {console.log(perfumeResults)}
+        {perfumeResults ? (
           <PerfumeIcon perfumes={perfumeResults} />
         ) : (
-          <p>Sorry, no such perfumes</p>
+          <p>Sorry, no such perfumes!</p>
         )}
       </div>
     </>
